6357334868e3412c157ac0ea82a483a315b418b3
Divyanshu Verma <divyanshuverma08@gmail.com>
2022-12-24 00:37:05 +0530

    first commit

---
+++ src/components/landingPage/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div style={{position:"fixed", bottom:0, visibility:"hidden"}} className=" mt-auto py-2 flex justify-center items-center bg-white shadow-inner">
+      <h1 className="text-base font-poppins font-semibold">
+       
+      </h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7dd3fc",
+        secondary: "#9CB2AA",
+        tertiary: "#B2D8CB",
+        bgprimary: "#FAFCFF",
+        bgsecondary: "#e0f2fe",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "ui-sans-serif"],
+        poppins: ["Poppins", "ui-sans-serif"],
+        lato: ["Lato", "SFMono-ui-monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
